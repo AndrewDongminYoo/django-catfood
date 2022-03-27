476d117eec7f4aee372248490db48a417f040320
dongminyoo <andrew@i-ve.kr>
2022-03-27 21:53:33 +0900

    feat(model): 브랜드 관련 모델 추가

--- catfood/models.py
+++ catfood/models.py
@@ -3,28 +3,48 @@ from django.db import models
 # Create your models here.
 
 
+class ProductType(models.TextChoices):
+    DRY = '건식사료', 'Dry'
+    WET = '습식사료', 'Wet'
+
+
+class ProductStatus(models.TextChoices):
+    NORMAL = '일반상품', 'Normal'
+    USED = '중고상품', 'Used'
+    SOLD_OUT = '단종상품', 'Sold Out'
+    BOOKABLE = '예정상품', 'Bookable'
+
+
+class ShoppingMall(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Maker(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=50)
+    maker = models.ForeignKey(Maker, null=True, blank=True, on_delete=models.CASCADE, verbose_name='제조사')
+
+    def __str__(self):
+        return self.name
+
+
 class NaverProduct(models.Model):
     product_id = models.IntegerField(verbose_name='상품 ID')
     title = models.CharField(max_length=100, verbose_name='상품명')
     link = models.URLField(verbose_name='상품 URL')
     image_url = models.URLField(verbose_name='상품 이미지 URL')
     low_price = models.IntegerField(null=True, blank=True, verbose_name='상품 할인가')
-    shopping_mall = models.CharField(max_length=100, verbose_name='쇼핑몰')
-    product_type = models.CharField(max_length=100, verbose_name='상품 종류')
-    brand = models.CharField(max_length=100, verbose_name='브랜드')
-    maker = models.CharField(max_length=100, verbose_name='제조사')
-    texture = models.CharField(max_length=100, verbose_name='타입')
-
-
-class ShoppingMall(models.Model):
-    mall_name = models.CharField(max_length=100, verbose_name='쇼핑몰명')
-    mall_url = models.URLField(verbose_name='쇼핑몰 URL')
-
-
-class ProductType(models.Model):
-    type_name = models.CharField(max_length=100, verbose_name='상품 종류')
-
-
-class Brand(models.Model):
-    brand_name = models.CharField(max_length=100, verbose_name='브랜드명')
-
+    shopping_mall = models.ForeignKey(ShoppingMall, null=True, blank=True, on_delete=models.CASCADE, verbose_name='판매사명')
+    product_status = models.CharField(max_length=100, choices=ProductStatus.choices, verbose_name='상품 상태')
+    brand = models.ForeignKey(Brand, null=True, blank=True, on_delete=models.CASCADE, verbose_name='브랜드명')
+    product_type = models.CharField(max_length=100, verbose_name='타입')
--- catfood/naver_search.py
+++ catfood/naver_search.py
@@ -15,58 +15,66 @@ from django_catfood import settings
 if 'setup' in dir(django):
     django.setup()
 
-from catfood.models import NaverProduct
+from catfood.models import NaverProduct, ShoppingMall, Brand, Maker
 from django.db.utils import IntegrityError
 
 PRODUCT_TYPE = {
-    '1': "가격비교상품",
-    '2': "일반비매칭상품",
-    '3': "일반매칭상품",
-    '4': "중고비교상품",
-    '5': "중고비매칭상품",
-    '6': "중고매칭상품",
-    '7': "단종비교상품",
-    '8': "단종비매칭상품",
-    '9': "단종매칭상품",
-    '10': "예정비교상품",
-    '11': "예정비매칭상품",
-    '12': "예정매칭상품",
+    '1': "일반상품",
+    '2': "일반상품",
+    '3': "일반상품",
+    '4': "중고상품",
+    '5': "중고상품",
+    '6': "중고상품",
+    '7': "단종상품",
+    '8': "단종상품",
+    '9': "단종상품",
+    '10': "예정상품",
+    '11': "예정상품",
+    '12': "예정상품",
 }
 
 
 def naver_shopping_search():
+    keywords = ["인도어"]
     start = 1
     DISPLAY_COUNT = 100
-    while True:
-        encText = quote("고양이 치킨")
-        url = f"https://openapi.naver.com/v1/search/shop?query={encText}&display={DISPLAY_COUNT}&start={start}"
-        headers = {
-            "X-Naver-Client-Id": settings.NAVER_ID,
-            "X-Naver-Client-Secret": settings.NAVER_SECRET
-        }
-        response = requests.get(url, headers=headers)
-        if start > 1000:
-            continue
-        start += DISPLAY_COUNT
-        body = response.json()
-        print(body)
-        for data in body["items"]:
-            print(data)
-            if data['category3'] == "고양이 사료":
-                naver_product = NaverProduct.objects.get_or_create(
-                    product_id=data['productId'],
-                )[0]  # '11858083484'
-                _title = re.sub("</*b>", "", data['title'])
-                naver_product.title = re.sub(r"[\[(]+\W+[])]+", "", _title)
-                naver_product.link = data['link']
-                naver_product.image_url = data['image']
-                naver_product.low_price = data['lprice']  # '5690'
-                naver_product.shopping_mall = data['mallName']  # '네이버'
-                naver_product.product_type = PRODUCT_TYPE[data['productType']]  # '1' - 가격 비교 상품
-                naver_product.brand = data['brand']  # '캐츠랑'
-                naver_product.maker = data['maker']  # '대주산업'
-                naver_product.texture = data['category4']  # '건식사료'
-                naver_product.save()
+    for keyword in keywords:
+        while True:
+            encText = quote("고양이 " + keyword + " 사료")
+            url = f"https://openapi.naver.com/v1/search/shop?query={encText}&display={DISPLAY_COUNT}&start={start}"
+            headers = {
+                "X-Naver-Client-Id": settings.NAVER_ID,
+                "X-Naver-Client-Secret": settings.NAVER_SECRET
+            }
+            response = requests.get(url, headers=headers)
+            if start > 1000:
+                continue
+            start += DISPLAY_COUNT
+            body = response.json()
+            for data in body["items"]:
+                print(data)
+                if data['category3'] == "고양이 사료":
+                    naver_product = NaverProduct.objects.get_or_create(
+                        product_id=data['productId'],
+                    )[0]  # '11858083484'
+                    _title = re.sub("</*b>", "", data['title'])
+                    naver_product.title = re.sub(r"[\[(]+\W+[])]+", "", _title)
+                    naver_product.link = data['link']
+                    naver_product.image_url = data['image']
+                    naver_product.low_price = data['lprice']  # '5690'
+                    if data['brand']:  # '캐츠랑'
+                        brand = Brand.objects.get_or_create(name=data['brand'])[0]
+                        naver_product.brand = brand
+                    if data['maker']:  # '대주산업'
+                        maker = Maker.objects.get_or_create(name=data['maker'])[0]
+                        if naver_product.brand:
+                            naver_product.brand.maker = maker
+                            naver_product.brand.save()
+                    if data["mallName"]:
+                        naver_product.shopping_mall = ShoppingMall.objects.get_or_create(name=data['mallName'])[0]  # '네이버'
+                    naver_product.product_status = PRODUCT_TYPE[data['productType']]  # '1' - 가격 비교 상품
+                    naver_product.product_type = data['category4']  # '건식사료'
+                    naver_product.save()
 
 
 if __name__ == '__main__':
